577135652b6c7d16e6bbefbaa4f567a2ac199b9b
JuanOrtiz <jsortiz502@misena.edu.co>
2022-11-08 15:49:53 -0500

    Subir sitio
    He subido los archivos de momento

---
+++ JS/questions.js
@@ -0,0 +1,19 @@
+(function(){
+    const titleQuestions = [...document.querySelectorAll(".questions-title")];
+
+    titleQuestions.forEach(questions =>{
+        questions.addEventListener('click', ()=>{
+            let height = 0;
+            let answer = questions.nextElementSibling;
+            let addPadding = questions.parentElement.parentElement;
+
+            addPadding.classList.toggle('questions-padding--add');
+            questions.children[0].classList.toggle('questions-arrow--rotate');
+
+            if (answer.clientHeight === 0) {
+                height = answer.scrollHeight;
+            }
+            answer.style.height = `${height}px`;
+        })
+    })
+})();
